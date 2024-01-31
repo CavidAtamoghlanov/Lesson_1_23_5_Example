9e806499c5c5d294912c8e11b6bbd003be2940ba
Siracli Elvin Intiqam <Sirac_xl04@itstep.edu.az>
2024-01-31 17:20:15 +0400

    Update 2 By2 Elvin

--- ConsoleApplication8/ConsoleApplication8.cpp
+++ ConsoleApplication8/ConsoleApplication8.cpp
@@ -1,44 +1,5 @@
 #include <iostream>
 
-using namespace std;
-
-
-class ClassByElvin
-{
-	string name = "Elvin";
-	int age = 16;
-
-public:
-
-	void Show()
-	{
-		cout << endl << name << endl << age << endl;
-	}
-};
-
-void Function_By_Elvin()
-{
-	cout << "\nFunction By Elvin" << endl << endl;
-
-}
-void Function_By_Huseyn()
-{
-	cout << "\nFunction By Huseyn" << endl << endl;
-
-}
-
-
-// Find Min
-int FindMin(int arr[], int size)
-{
-	int min = arr[0];
-	for (int i = 1; i < size; i++)
-	{
-		if (arr[i] < min)
-			min = arr[i];
-	}
-	return min;
-}
 
 
 // Find Max Element
