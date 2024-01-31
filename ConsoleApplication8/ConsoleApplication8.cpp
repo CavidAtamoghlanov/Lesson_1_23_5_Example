0726964a76007d7f9315dd8ccab282519468ed5d
MirtalibEmirli <azad.sdk.85@icloud.com>
2024-01-31 17:01:58 +0400

    Maksimumun tapilmasi hazirdir dostlar

--- ConsoleApplication8/ConsoleApplication8.cpp
+++ ConsoleApplication8/ConsoleApplication8.cpp
@@ -7,6 +7,18 @@ void FoobyElvin()
     cout << "Elvin" << endl;
 }
 
+// Find Max Element
+int FindMax(int arr[], int size)
+{
+	int max = arr[0];
+	for (int i = 1; i < size; i++)
+	{
+		if (arr[i] > max)
+			max = arr[i];
+	}
+	return max;
+}
+
 int main()
 {
     cout << "Hello World!\n";
